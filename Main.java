72d89c52f8f19ddb6b7f5d3aa57e95fd52f18b47
Saurabh Jagadale <saurabhjagadale98@gmail.com>
2022-03-10 12:44:45 +0530

    Master commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Debugger.
+                Code, Run and Debug Java program online.
+Write your code in this editor and press "Debug" button to debug program.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+   
+     int x = 001, y=010, z=100;
+        System.out.println("x="+x);//1
+        System.out.println("y="+y);//8
+        System.out.println("z="+z);//100
+        
+        int i = --x + y++ - z-- - --z + ++y - --x + y-- - --x;
+        //      0   + 8   - 100 - 98  + 10  - -1  + 10  - -2  
+      
+         
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+        System.out.println("i="+i);
+                                       
+	}
+}
